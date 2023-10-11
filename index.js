f7bc0de619047bc3944c6287722716f2cbc0d44a
rrmc <rrmc.tapia@yahoo.com>
2023-10-10 18:43:12 -0700

    updated

--- index.js
+++ index.js
@@ -8,35 +8,35 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date?", function(req, res) {
   //check if in date format or UNIX format
   const dateString = req.params.date;
   let date;
-  if( !dateString ){
+  if (!dateString) {
     date = new Date();
-  }else{
-    if(!isNaN(dateString)){
+  } else {
+    if (!isNaN(dateString)) {
       date = new Date(parseInt(dateString));
-    }else{
-      date  = new Date(dateString);
+    } else {
+      date = new Date(dateString);
     }
   }
 
-  if(date.toString() == 'Invalid Date'){
-    res.json({error: date.toString});
-  }else{
+  if (date.toString() == 'Invalid Date') {
+    res.json({ error: date.toString() });
+  } else {
     res.json({
       "unix": date.getTime(),
       "utc": date.toUTCString()
@@ -48,6 +48,6 @@ app.get("/api/:date", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen( 3000 , function () {
+var listener = app.listen(3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
