a9adc2894201cb1ed91e4d32f1206e82685cff85
rrmc <rrmc.tapia@yahoo.com>
2023-10-10 18:31:34 -0700

    complete

--- index.js
+++ index.js
@@ -20,13 +20,34 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  //check if in date format or UNIX format
+  const dateString = req.params.date;
+  let date;
+  if( !dateString ){
+    date = new Date();
+  }else{
+    if(!isNaN(dateString)){
+      date = new Date(parseInt(dateString));
+    }else{
+      date  = new Date(dateString);
+    }
+  }
+
+  if(date.toString() == 'Invalid Date'){
+    res.json({error: date.toString});
+  }else{
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    })
+  }
+
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen( 3000 , function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
